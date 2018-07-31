57630e5c4d8e78755ac3d8b0ef4df63136966fa6
elpuas <elpuas@gmail.com>
2018-07-30 20:51:12 -0400

    move buttons to heaader

--- assets/safed-scripts.js
+++ assets/safed-scripts.js
@@ -1,3 +1,19 @@
 jQuery(document).ready(function($){
+
+    // Check if Element Exist in the DOM
+    $.fn.exists = function(callback) {
+        var args = [].slice.call(arguments, 1);
+      
+        if (this.length) {
+          callback.call(this, args);
+        }
+      
+        return this;
+    };
+
+    $('.page-business-driving-records-online .button_wrapper').exists(function(){
+        this.appendTo('.page-business-driving-records-online section .usa-grid');
+    })
+
     console.log("%c Made with  💖 and a lot of  ☕ by el.puas | https://elpuas.com ", "color:#fff;background:gold;");
 });
